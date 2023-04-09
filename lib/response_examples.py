6829fb15787fda7e1b3bc7f1b5778c74651d78e0
windownik <windownik@gmail.com>
2023-04-09 20:59:55 +0300

    First commit

---
+++ lib/response_examples.py
@@ -0,0 +1,192 @@
+get_me_res = {
+    200: {
+        "description": "Success",
+        "content": {
+            "application/json": {
+                "examples": {
+                    "odd": {
+                        "summary": "Success",
+                        "value": {"ok": True,
+                                  'user_id': 12,
+                                  'name': 'Anna',
+                                  'surname': 'Volkova',
+                                  'phone': 321161666,
+                                  'email': 'anna@gmail.com',
+                                  'status': 'creator',
+                                  'last_active': '2023-01-17 21:54:23.738397',
+                                  }
+                    },
+                }
+            }
+        }
+    },
+    401: {
+        "description": "Success",
+        "content": {
+            "application/json": {
+                "examples": {
+                    "odd": {
+                        "summary": "Success",
+                        "value": 'bad access token'
+                    },
+                }
+            }
+        }
+    },
+}
+
+access_token_res = {
+    200: {
+        "description": "Success",
+        "content": {
+            "application/json": {
+                "examples": {
+                    "odd": {
+                        "summary": "Success",
+                        "value": {"ok": True,
+                                  'user_id': 12,
+                                  'access_token': 'fFsok0mod3y5mgoe203odk3f'}
+                    },
+                }
+            }
+        }
+    },
+    401: {
+        "description": "Success",
+        "content": {
+            "application/json": {
+                "examples": {
+                    "odd": {
+                        "summary": "Success",
+                        "value": 'bad refresh token, please login'
+                    },
+                }
+            }
+        }
+    },
+}
+
+check_phone_res = {
+    200: {
+        "description": "Success",
+        "content": {
+            "application/json": {
+                "examples": {
+                    "odd": {
+                        "summary": "Success",
+                        "value": {"ok": True, 'desc': 'no phone in database'}
+                    },
+                }
+            }
+        }
+    },
+    226: {
+        "description": "Success",
+        "content": {
+            "application/json": {
+                "examples": {
+                    "odd": {
+                        "summary": "Success",
+                        "value": 'have user with same phone'
+                    },
+                }
+            }
+        }
+    },
+}
+
+create_user_res = {
+    200: {
+        "description": "Success",
+        "content": {
+            "application/json": {
+                "examples": {
+                    "odd": {
+                        "summary": "Success",
+                        "value": {"ok": True,
+                                  'user_id': 1,
+                                  'access_token': '123',
+                                  'refresh_token': '123'}
+                    },
+                }
+            }
+        }
+    },
+}
+
+update_user_res = {
+    200: {
+        "description": "Success",
+        "content": {
+            "application/json": {
+                "examples": {
+                    "odd": {
+                        "summary": "Success",
+                        "value": {"ok": True,
+                                  'desc': 'all users information updated'}
+                    },
+                }
+            }
+        }
+    },
+}
+
+login_res = {
+    200: {
+        "description": "Success",
+        "content": {
+            "application/json": {
+                "examples": {
+                    "odd": {
+                        "summary": "Success",
+                        "value": {"ok": True,
+                                  'desc': 'all users information updated'}
+                    },
+                }
+            }
+        }
+    },
+    401: {
+        "description": "Success",
+        "content": {
+            "application/json": {
+                "examples": {
+                    "odd": {
+                        "summary": "Success",
+                        "value": "bad phone or password"
+                    },
+                }
+            }
+        }
+    },
+}
+
+update_pass_res = {
+    200: {
+        "description": "Success",
+        "content": {
+            "application/json": {
+                "examples": {
+                    "odd": {
+                        "summary": "Success",
+                        "value": {"ok": True,
+                                  'desc': 'all users information updated'}
+                    },
+                }
+            }
+        }
+    },
+    401: {
+        "description": "Success",
+        "content": {
+            "application/json": {
+                "examples": {
+                    "odd": {
+                        "summary": "Success",
+                        "value": "bad phone or password"
+                    },
+                }
+            }
+        }
+    },
+}
