0e55d8b01e529d13aff951c4ca8a409ce7eb0bef
windownik <windownik@gmail.com>
2023-04-18 00:24:40 +0300

    deploy with docker

--- lib/routes/push/push_func.py
+++ lib/routes/push/push_func.py
@@ -3,7 +3,7 @@ import firebase_admin
 from firebase_admin import messaging
 from firebase_admin import credentials
 
-cred = credentials.Certificate("svddoc-fcm-admin.json")
+cred = credentials.Certificate("cleaner-app.json")
 firebase_admin.initialize_app(cred)
 
 
@@ -20,3 +20,7 @@ def send_push(fcm_token: str, title: str, body: str, main_text: str, push_type: 
         token=fcm_token
     )
     response = messaging.send(message)
+
+
+send_push(fcm_token='cLJnxualQXGqcFrFpM29EA:APA91bFvBA6UVeX3y-QVo5XgeT_7vaTZsCoqE4iKFwN-FnTTlMGMLb1xlVEv9OGJNdiKSFDC3MxJkBtcsFw84FVLymSkxSp1TSe_32Ef2e1yN5gpLnYKKnu67x8jIYvxBZLy-ltRZTPF',
+          title='Test text', body='11111111111', main_text='hi text', push_type='ewwrfvfrvd')
